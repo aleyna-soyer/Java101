85a83e8114078b725980e70c95633e7bdfe05dc0
aleyna-soyer <aleynasoyer9@gmail.com>
2023-02-17 01:01:17 +0300

    Variables

---
+++ src/Lesson2.java
@@ -0,0 +1,22 @@
+
+public class Lesson2 {
+
+	public static void main(String[] args) {
+		
+		int a=77;
+		short s=32;
+		double d=3.14;
+		char c='A';
+		boolean b= true;
+		String x="ALeyna";
+		
+		System.out.println("the value of a is "+ a);
+		System.out.println(s);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(x);
+				
+				
+	}
+} 
