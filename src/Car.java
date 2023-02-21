20736c5105d2d8f6c0c918963239220df79a355b
aleyna-soyer <aleynasoyer9@gmail.com>
2023-02-21 21:46:39 +0300

    get-set

--- src/Car.java
+++ src/Car.java
@@ -2,9 +2,40 @@
 public class Car {
 
 	//Fields, properties
-	public String color;
-	public String model;
-	public double engine;
-	public int doors;
+	private String color;
+	private String model;
+	private double engine;
+	private int doors;
+	
+	public String getColor() {
+		return this.color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getModel() {
+		return this.model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public double getEngine() {
+		return this.engine;
+	}
+	public void setEngine(double engine) {
+		this.engine = engine;
+	}
+	public int getDoors() {
+		return this.doors;
+	}
+	public void setDoors(int doors) {
+		this.doors = doors;
+	}
+	public void start() {
+		System.out.println("Car started..");
+	}
+	public void stop() {
+		System.out.println("Car stopped...");
+	}
 	
 	}
