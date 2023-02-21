42b3622a743abc12b18ced6757e3251ce40f6052
aleyna-soyer <aleynasoyer9@gmail.com>
2023-02-21 21:03:18 +0300

    Class

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+public static void main(String[] args) {
+	Car car1=new Car();
+	
+	car1.color="Red";
+	car1.model="Audi A6";
+	car1.engine=1.6;
+	car1.doors=4;
+	
+	System.out.println("Color: "+ car1.color);
+	System.out.println("Model: "+ car1.model);
+	System.out.println("Engine: "+ car1.engine);
+	System.out.println("Doors: "+ car1.doors);
+	
+	
+	
+}
+}
