2484195d4fa8d3497969041dd2b8321eef84b34c
aleyna-soyer <aleynasoyer9@gmail.com>
2023-02-22 23:53:17 +0300

    Multidimensional arrays

---
+++ src/Lesson16.java
@@ -0,0 +1,24 @@
+
+public class Lesson16 {
+	public static void main(String[] args) {
+		int[][] matrix=new int[3][4];
+		int number=1;
+		//row
+		for(int i=0; i<matrix.length; i++) {
+			//column
+			for(int j=0; j<matrix[i].length; j++) {
+				matrix[i][j]=number++;
+				
+			}
+		}
+		for(int i=0; i<matrix.length; i++) {
+			for(int j=0; j<matrix[i].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+}
